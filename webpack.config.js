c50dd782b96a04a172aeae97bd20f70d9bc54209
ZiedDev <73042811+ZiedDev@users.noreply.github.com>
2024-01-21 17:32:38 +0200

    make Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    // devtool: 'inline-source-map',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    }
+};
