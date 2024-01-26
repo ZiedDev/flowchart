fe0dd59da1ae279c92f2cf9f5ca0a525a0a84a69
ZiedDev <73042811+ZiedDev@users.noreply.github.com>
2024-01-26 17:45:56 +0200

    add side panel to add blocks and add tools (only hand working atm)

--- src/js/index.js
+++ src/js/index.js
@@ -14,19 +14,25 @@ const processTemplate = document.getElementById('process-template')
 const decisionTemplate = document.getElementById('decision-template')
 const inputOutputTemplate = document.getElementById('input-output-template')
 
-const start = createFlowBlock('terminal', 'start', 'start')[1]
-const input = createFlowBlock('inputOutput', 'input', 'INPUT X',)[1]
-const condition = createFlowBlock('decision', 'condition', 'X > 5')[1]
-const end = createFlowBlock('terminal', 'end', 'end')[1]
+// variables
+const draggables = []
 
-const line1 = createLine('start', 'input', start, input)
-const line2 = createLine('input', 'condition', input, condition)
-const line3 = createLine('condition', 'end', condition, end)
+// hard coded
+const start = createFlowBlock('terminal', '0', 'start', flowchartBoard, true)[1]
+const input = createFlowBlock('inputOutput', '1', 'INPUT X', flowchartBoard, true)[1]
+const condition = createFlowBlock('decision', '2', 'X > 5', flowchartBoard, true)[1]
+const end = createFlowBlock('terminal', '3', 'end', flowchartBoard, true)[1]
 
+const line1 = createLine('0', '1')
+const line2 = createLine('1', '2')
+const line3 = createLine('2', '3')
+
+updateFlowBlock(start, 'onMove', () => { line1.position() })
 updateFlowBlock(input, 'onMove', () => { line1.position(), line2.position() })
 updateFlowBlock(condition, 'onMove', () => { line2.position(), line3.position() })
+updateFlowBlock(end, 'onMove', () => { line3.position() })
 
-function createFlowBlock(symbolType, id, content) {
+function createFlowBlock(symbolType, id, content, draggable) {
     const flowchartSymbols = ['terminal', 'process', 'decision', 'inputOutput']
 
     let element
@@ -50,11 +56,13 @@ function createFlowBlock(symbolType, id, content) {
         element = inputOutputTemplate.content.cloneNode(true)
         element.querySelector('[data-flowchart-block-id]').id = id
         element.querySelector('[data-flowchart-block-content]').textContent = content
-
     }
+
+    if (!draggable) return element
 
     flowchartBoard.appendChild(element)
     const elementDraggable = new PlainDraggable(document.getElementById(id))
+    draggables.push(elementDraggable)
 
     return [element, elementDraggable]
 }
@@ -63,11 +71,73 @@ function updateFlowBlock(flowBlock, func, whatToUpdate) {
     flowBlock[func] = whatToUpdate
 }
 
-function createLine(start, end, startNode, endNode) {
+function createLine(start, end) {
     const line = new LeaderLine(document.getElementById(start), document.getElementById(end), { startPlug: 'disc', size: 4, startPlugSize: 1.5, startPlugOutlineSize: 2.5, color: '#f0f8ff', path: 'fluid' })
-
-    startNode.onMove = () => { line.position() }
-    endNode.onMove = () => { line.position() }
-
     return line
+}
+
+// addBlockPanel
+const addBlocksPanel = document.getElementById('add-blocks-panel')
+const addPanelBlocks = [['start', 'terminal'], ['end', 'terminal'], ['input', 'inputOutput'], ['output', 'inputOutput'], ['condition', 'decision']]
+const addPanelElements = createBlocksPanel(addPanelBlocks)
+
+for (let i = 0; i < addPanelElements.length; i++) {
+    const ele = addPanelElements[i]
+    addBlocksPanel.appendChild(ele[0])
+    document.getElementById(ele[1]).addEventListener('click', () => createFlowBlock(ele[2], `${Math.floor(Math.random() * 99999)}`, '...', true))
+}
+
+function createBlocksPanel(panelBlocks = []) {
+    const allBlocks = []
+
+    for (let i = 0; i < panelBlocks.length; i++) {
+        const element = createFlowBlock(panelBlocks[i][1], panelBlocks[i][0], panelBlocks[i][0], false)
+        allBlocks.push([element, panelBlocks[i][0], panelBlocks[i][1]])
+    }
+
+    return allBlocks
+}
+
+// building mode
+const blocksButton = document.getElementById('blocks-button')
+const wiresButton = document.getElementById('wires-button')
+const moveButton = document.getElementById('move-button')
+
+blocksButton.addEventListener('click', () => {
+    toggleSwitch([blocksButton, wiresButton, moveButton], 0, 'active', () => {
+        toggleDraggables(false)
+    })
+})
+
+wiresButton.addEventListener('click', () => {
+    toggleSwitch([blocksButton, wiresButton, moveButton], 1, 'active', () => {
+        toggleDraggables(false)
+    })
+})
+moveButton.addEventListener('click', () => {
+    toggleSwitch([blocksButton, wiresButton, moveButton], 2, 'active', () => {
+        toggleDraggables(true)
+    })
+})
+
+function toggleSwitch(toggleSwitches = [], index, activeClass, onActivateFunc = () => { }) {
+    for (let i = 0; i < toggleSwitches.length; i++) {
+        toggleSwitches[i].classList.remove(activeClass)
+    }
+
+    toggleSwitches[index].classList.add(activeClass)
+    onActivateFunc()
+}
+
+function toggleDraggables(enable) {
+    if (!enable) {
+        for (let i = 0; i < draggables.length; i++) {
+            draggables[i].disabled = true
+        }
+    }
+    else {
+        for (let i = 0; i < draggables.length; i++) {
+            draggables[i].disabled = false
+        }
+    }
 }
